6b30845d50c70e8256ced1dcddda683ced4049a8
Iza <mendes.izabel08@gmail.com>
2022-07-21 19:46:50 -0300

    testes feitos. funcionando local

--- testesaction/testesaction/tests.py
+++ testesaction/testesaction/tests.py
@@ -1,6 +1,30 @@
+from tokenize import Number
 import unittest
+from xmlrpc.client import boolean
 
-class FirstTest (unittest.TestCase):
+#import TestCase
+
+
+#biblioteca python para testes unitários
+# TestCase ferramenta do django para testes automatizados
+
+class ActionsTest (unittest.TestCase):
 
     def test_hello_world(self):
-        assert True
+        x = 'dev web'
+        self.assertEqual(x.split(), ['dev', 'web'])
+        with self.assertRaises(TypeError):
+            x.split(2)
+        #deverá retornar verdadeiro pois faz a chegarem a função x.split se funciona quando o separador não é string
+
+    def test_Upper(self):
+        self.assertEqual('izabel'.upper(), 'IZABEL')
+        #deverá retornar verdadeiro pois o .upper vai transformar o nome em questão em caixa alta, logo são iguais
+
+    def test_islower(self):
+        self.assertFalse('ISLOWER'.islower())
+        #deverá retornar verdadeiro pois a afirmação é falsa
+
+    def test_boolean(self):
+        y = 3
+        self.assertNotEquals(y, 'tres')
